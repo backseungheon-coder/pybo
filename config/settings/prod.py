f3e65cd67166fab9d7b2633960d6e5e494f755f6
dorlback <bsh7829@gmail.com>
2022-03-09 21:06:00 +0900

    도메인변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['52.79.210.1']
+ALLOWED_HOSTS = ['52.79.210.1','dorback98.duckdns.org']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
